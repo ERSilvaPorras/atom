e74b79d459dd801d7bb0963ffdbb20a866fcc000
ERSilvaPorras <eduasilvaporras@frba.utn.edu.ar>
2026-04-23 11:53:22 -0300

    feat(cli): update version command

--- lib/version.sh
+++ lib/version.sh
@@ -5,5 +5,6 @@ set -eu
 INSTALL_DIR="$HOME/.atom"
 
 version() {
-    git -C "$INSTALL_DIR" describe --tags --abbrev=0 || echo "v0.0.0"
+    VERSION=$(git -C "$INSTALL_DIR" describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
+    echo "atom version $VERSION"
 }
