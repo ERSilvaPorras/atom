1958ebd1dc928ccaebabf6b45c144991a28ec8b5
ERSilvaPorras <eduasilvaporras@frba.utn.edu.ar>
2026-04-26 01:55:38 -0300

    fix(uninstall): remove atom from /usr/local/bin when ~/.local/bin is not in PATH

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,10 @@ set -eu
 BIN_PATH="$HOME/.local/bin/atom"
 INSTALL_DIR="$HOME/.atom"
 
+if ! echo "$PATH" | grep -q "$HOME/.local/bin"; then
+    BIN_PATH="/usr/local/bin/atom"
+fi
+
 sudo rm -f "$BIN_PATH"
 sudo rm -rf "$INSTALL_DIR"
 
