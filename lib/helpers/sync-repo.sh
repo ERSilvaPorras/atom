0efb2f7f741317426a72464a4034d38992f2d0f1
ERSilvaPorras <eduasilvaporras@frba.utn.edu.ar>
2026-04-26 14:19:14 -0300

    fix(sync-repo): prompt for SSH key name before syncing repository

--- lib/helpers/sync-repo.sh
+++ lib/helpers/sync-repo.sh
@@ -6,12 +6,13 @@ sync_repo() {
     . "$SCRIPT_DIR/../lib/helpers/colors.sh"
     REPO_DIR=${1:-}
     TARGET_FILES=${2:-"."}
+    read -p "Entre name SSH KEY: " SSH_NAME
     read -p "Enter message of commit: " MSG_COMMIT
 
     if ! git -C "$REPO_DIR" diff --quiet "$TARGET_FILES"; then
         git -C "$REPO_DIR" add "$TARGET_FILES"
         . "$SCRIPT_DIR/../lib/commands/sshup.sh"
-        sshup "$1"
+        sshup "$SSH_NAME"
         git -C "$REPO_DIR" commit -m "$MSG_COMMIT"
         git -C "$REPO_DIR" push origin main
         printf "\t[OK] Extensions exported and pushed to repository successfully 🚀\n"
