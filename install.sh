71ca95bc42c9610d40f9bfa4c8a667a38c02e125
ERSilvaPorras <eduasilvaporras@frba.utn.edu.ar>
2026-04-26 01:43:42 -0300

    fallback to /usr/local/bin when ~/.local/bin is not in PATH

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ BIN_PATH="$HOME/.local/bin/atom"
 
 echo "[INFO] Installing Atom CLI ..."
 
-# Clone (solo deseo clonar, lo de actualizar lo quiero hacer manualmente)
 if [ ! -d "$INSTALL_DIR" ]; then 
     echo "[INFO] Cloning Atom repository to $INSTALL_DIR ..."
     git clone "$REPO_URL" "$INSTALL_DIR"
@@ -17,35 +16,22 @@ fi
 # permissions
 chmod +x "$INSTALL_DIR/bin/atom"
 
+# Verify if ~/.local/bin is in PATH
+if ! echo "$PATH" | grep -q "$HOME/.local/bin"; then
+    BIN_PATH="/usr/local/bin/atom"
+    echo "[INFO] Will create symlink in /usr/local/bin"
+fi
+
 # Symlink global
 echo "[INFO] Creating symlink ..."
 if [ ! -d "$BIN_PATH" ]; then
     mkdir -p "$(dirname "$BIN_PATH")"
 fi
-ln -sf "$INSTALL_DIR/bin/atom" "$BIN_PATH"
 
-# Add to PATH
-if ! echo "$PATH" | grep -q "$HOME/.local/bin"; then
-    # Identificar la shell actual
-    CURRENT_SHELL=$(basename "$SHELL")
-    case "$CURRENT_SHELL" in
-        bash)
-            echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
-            . "$HOME/.bashrc"
-            ;;
-        zsh)
-            echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.zshrc"
-            . "$HOME/.zshrc"
-            ;;
-        sh)
-            echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.profile"
-            . "$HOME/.profile"
-            ;;
-        *)
-            echo "[ERROR] Unsupported shell: $CURRENT_SHELL. Please add $HOME/.local/bin to your PATH manually."
-            exit 1
-            ;;
-    esac
+if [ "$BIN_PATH" == "/usr/local/bin/atom" ]; then
+    sudo ln -sf "$INSTALL_DIR/bin/atom" "$BIN_PATH"
+else
+    ln -sf "$INSTALL_DIR/bin/atom" "$BIN_PATH"
 fi
 
 echo "[OK] Successfully installed 🚀"
